81de147ad85c7f7fd952e4de728a259cae0f0eda
minerva421 <minerva421.ai@gmail.com>
2022-10-23 00:40:05 +0900

    user에 email field 추가

--- community/user/admin.py
+++ community/user/admin.py
@@ -5,6 +5,6 @@ from .models import Fcuser
 # Register your models here.
 
 class FcuserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'password', 'registered_dttm')
+    list_display = ('username', 'useremail', 'password', 'registered_dttm')
 
 admin.site.register(Fcuser, FcuserAdmin)
--- community/user/views.py
+++ community/user/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from urllib import response
+from django.shortcuts import HttpResponse, render
+from django.contrib.auth.hashers import make_password
+from .models import Fcuser
 
 # Create your views here.
 
+
 def regitster(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re_password', None)
+
+        res_data = {}
+
+        if not (username and useremail and password and re_password):
+            res_data['error'] = '모든 값을 입력해야 합니다'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다'
+        else:
+            fcuser = Fcuser(
+                username=username,
+                useremail=useremail,
+                password=make_password(password)
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
